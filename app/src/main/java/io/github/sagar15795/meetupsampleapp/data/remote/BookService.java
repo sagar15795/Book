254d0e3ac1e9a98b1baab98b22f3109978eaeacf
Sagar <kumarsagar15795@gmail.com>
2018-08-03 16:56:12 +0530

    added getbook endpoint to service

--- app/src/main/java/io/github/sagar15795/meetupsampleapp/data/remote/BookService.java
+++ app/src/main/java/io/github/sagar15795/meetupsampleapp/data/remote/BookService.java
@@ -1,6 +1,14 @@
 package io.github.sagar15795.meetupsampleapp.data.remote;
 
+import java.util.List;
+
+import io.github.sagar15795.meetupsampleapp.data.model.Book;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface BookService {
 
+    @GET("/book")
+    Call<List<Book>> getBook();
 
 }
