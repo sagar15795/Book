3c4faa89294c2899051704d3cfbbad64d2c3c288
Sagar <kumarsagar15795@gmail.com>
2018-08-03 17:13:02 +0530

    added api endpoint for add book

--- app/src/main/java/io/github/sagar15795/meetupsampleapp/data/remote/BookService.java
+++ app/src/main/java/io/github/sagar15795/meetupsampleapp/data/remote/BookService.java
@@ -3,12 +3,19 @@ package io.github.sagar15795.meetupsampleapp.data.remote;
 import java.util.List;
 
 import io.github.sagar15795.meetupsampleapp.data.model.Book;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 
 public interface BookService {
 
     @GET("/book")
     Call<List<Book>> getBook();
 
+    @POST("/book")
+    @Headers("Content-Type: application/json")
+    Call<ResponseBody> addBook(@Body Book book);
 }
