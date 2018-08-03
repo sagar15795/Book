bb83a0392a311f76246180cea72082f5da270452
Sagar <kumarsagar15795@gmail.com>
2018-08-03 16:56:36 +0530

    added get book api to the main activity

--- app/src/main/java/io/github/sagar15795/meetupsampleapp/main/MainActivity.java
+++ app/src/main/java/io/github/sagar15795/meetupsampleapp/main/MainActivity.java
@@ -1,14 +1,15 @@
 package io.github.sagar15795.meetupsampleapp.main;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,9 @@ import io.github.sagar15795.meetupsampleapp.R;
 import io.github.sagar15795.meetupsampleapp.data.model.Book;
 import io.github.sagar15795.meetupsampleapp.data.remote.BookAPIManager;
 import io.github.sagar15795.meetupsampleapp.utils.RecyclerItemClickListner;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements RecyclerItemClickListner.OnItemClickListener {
     private RecyclerView recyclerView;
@@ -37,7 +41,32 @@ public class MainActivity extends AppCompatActivity implements RecyclerItemClick
     @Override
     protected void onResume() {
         super.onResume();
+        bookAPIManager = new BookAPIManager();
+        bookAPIManager.getBookApi().getBook().enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+                if (response.body() == null || response.body().size()==0) {
+                    Toast.makeText(MainActivity.this, "No Book Found", Toast.LENGTH_SHORT).show();
+                } else {
+                    bookList.clear();
+                    bookList.addAll(response.body());
+                    bookAdapter.notifyDataSetChanged();
+                }
+            }
 
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (!bookAPIManager.getBookApi().getBook().isExecuted()) {
+            bookAPIManager.getBookApi().getBook().cancel();
+        }
     }
 
     private void viewSetup() {
